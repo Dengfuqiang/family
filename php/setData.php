ad3fb4852b94b8166134e00603f32e9b1f0cd537
Dengfuqiang <244146962@qq.com>
2017-04-13 07:09:39 +0800

    add

---
+++ php/setData.php
@@ -0,0 +1,18 @@
+<?php
+	require_once 'mysql.class.php';
+	header("content-type:text/html;charset=utf-8");
+	 $filename = "test.json";
+     $json_string = file_get_contents($filename);
+	 $obj=json_decode($json_string,true);
+     if (!is_array($obj)) die('no successful');
+	 foreach ($obj['list']['pageList'] as $key => $value) {
+	 	$mysql = new MySQL('localhost','root','','family');
+		 var_dump($value);
+		$result= $mysql->data($value)->table('beautity_life')->add();
+	 	if($result>0){
+	 		echo '成功';
+	 	}else{
+	 		echo '失败';
+	 	}
+	 }
+?>
